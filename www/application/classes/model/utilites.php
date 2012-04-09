ef866ec9e6a76bb2c34971247540a33c7338c230
ykorvel <yury.korvel@itrexgroup.com>
2012-04-09 18:01:33 +0200

    Bug fixing

--- www/application/classes/model/utilites.php
+++ www/application/classes/model/utilites.php
@@ -2,7 +2,7 @@
 
 class Model_Utilites extends Model {
 
-    private $hashMethod = 'sha256'; // You hash method (must be the same with main configuration (config/auth.php))
+    private $hashMethod = 'sha256'; // Your hash method (must be the same with main configuration (config/auth.php))
     private $hashKey = '1, 3, 4, 6, 9, 13, 17, 20, 25, 30, 32, 40, 61'; // Your hash string (must be the same with main configuration (config/auth.php))
     private $chasher1 = array("1", "2", "3", "4", "5", "6", "7", "8", "9", "0", "a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z", "_", "-", "?", "#", "<", ">", "&", "=", ".", " ");
     private $chasher2 = array("YSFT", "P8TS", "QNIS", "XJA6", "GQUA", "IAMO", "7WPB", "SK8Z", "NA00", "X_T5", "Z__H", "A192", "BB97", "ALZ1", "1ZHA", "BAAA", "N98&", "XUAS", "776A", "YZ8A", "9_Y-", "ASVF", "AHH7", "577P", "BSM-", "ZHAG", "S98T", "QQW8", "CIO1", "9ADP", "VBZ9", "CBAJ", "166H", "HDAO", "Sl9k", "__US", "SAU7", "9823", "H772", "KDSP", "C091", "5-NN", "7SG1", "HSS7", "IO06", "4WW6");
