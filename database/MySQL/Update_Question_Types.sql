9db026d4567d48a0676f0225a9ef24794362a211
a-zharkov <alexander.zharkov@itrexgroup.com>
2013-04-05 11:31:26 +0300

    Question Module - New Features

--- database/MySQL/Update_Question_Types.sql
+++ database/MySQL/Update_Question_Types.sql
@@ -3,3 +3,7 @@ INSERT INTO `map_question_types` (`id`, `title`, `value`, `template_name`, `temp
 INSERT INTO `map_question_types` (`id`, `title`, `value`, `template_name`, `template_args`) VALUES (2, 'multi-line text entry - not assessed',	'area', 'area', NULL);
 INSERT INTO `map_question_types` (`id`, `title`, `value`, `template_name`, `template_args`) VALUES (3, 'multiple choice',	'mcq', 'choise', 0);
 INSERT INTO `map_question_types` (`id`, `title`, `value`, `template_name`, `template_args`) VALUES (4, 'pick choise',	'pqc', 'choise', 0);
+
+ALTER TABLE `map_questions` ADD `show_submit` tinyint(4) NOT NULL DEFAULT '0',
+ADD `redirect_node_id` int(10) unsigned DEFAULT NULL,
+ADD `submit_text` varchar(200) DEFAULT NULL;
