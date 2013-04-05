a1fbe84bf5ab1c92a58e29aab16ca4910ae9d099
a-zharkov <alexander.zharkov@itrexgroup.com>
2013-04-05 12:19:27 +0300

    Fixed SQL script.

--- database/MySQL/Update_Question_Types.sql
+++ database/MySQL/Update_Question_Types.sql
@@ -1,6 +1,6 @@
-DELETE FROM `map_question_types`;
-INSERT INTO `map_question_types` (`id`, `title`, `value`, `template_name`, `template_args`) VALUES (1, 'single line text entry - not assessd',	'text', 'text', NULL);
-INSERT INTO `map_question_types` (`id`, `title`, `value`, `template_name`, `template_args`) VALUES (2, 'multi-line text entry - not assessed',	'area', 'area', NULL);
+TRUNCATE TABLE `map_question_types`;
+INSERT INTO `map_question_types` (`id`, `title`, `value`, `template_name`, `template_args`) VALUES (1, 'single line text entry',	'text', 'text', NULL);
+INSERT INTO `map_question_types` (`id`, `title`, `value`, `template_name`, `template_args`) VALUES (2, 'multi-line text entry',	'area', 'area', NULL);
 INSERT INTO `map_question_types` (`id`, `title`, `value`, `template_name`, `template_args`) VALUES (3, 'multiple choice',	'mcq', 'choise', 0);
 INSERT INTO `map_question_types` (`id`, `title`, `value`, `template_name`, `template_args`) VALUES (4, 'pick choise',	'pqc', 'choise', 0);
 
