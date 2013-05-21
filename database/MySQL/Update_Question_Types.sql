335bd369263db12cd49a5ff7f854f932cc9a8e99
a-sherstnev <alexander.sherstnev@itrexgroup.com>
2013-05-21 15:52:23 +0300

    Node Grid in tabular format
    Bug fixing

--- database/MySQL/Update_Question_Types.sql
+++ database/MySQL/Update_Question_Types.sql
@@ -1,7 +1,7 @@
 TRUNCATE TABLE `map_question_types`;
 INSERT INTO `map_question_types` (`id`, `title`, `value`, `template_name`, `template_args`) VALUES (1, 'single line text entry',	'text', 'text', NULL);
 INSERT INTO `map_question_types` (`id`, `title`, `value`, `template_name`, `template_args`) VALUES (2, 'multi-line text entry',	'area', 'area', NULL);
-INSERT INTO `map_question_types` (`id`, `title`, `value`, `template_name`, `template_args`) VALUES (3, 'multiple choice',	'mcq', 'choise', 0);
+INSERT INTO `map_question_types` (`id`, `title`, `value`, `template_name`, `template_args`) VALUES (3, 'multiple choice',	'mcq', 'choice', 0);
 INSERT INTO `map_question_types` (`id`, `title`, `value`, `template_name`, `template_args`) VALUES (4, 'pick choise',	'pqc', 'choise', 0);
 
 ALTER TABLE `map_questions` ADD `show_submit` tinyint(4) NOT NULL DEFAULT '0',
