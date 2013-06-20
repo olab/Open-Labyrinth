9a253e01abce446fa6455c92d0dbbacae1813965
a-sherstnev <alexander.sherstnev@itrexgroup.com>
2013-06-20 10:36:46 +0300

    Single & multiple-line questions: updating "prompt text"

--- www/application/views/labyrinth/question/area.php
+++ www/application/views/labyrinth/question/area.php
@@ -62,7 +62,10 @@ if (isset($templateData['map'])) { ?>
             <label for="fback" class="control-label"><?php echo __('Prompt text'); ?>
             </label>
             <div class="controls">
-                <textarea id="fback" name="fback"><?php if(isset($templateData['question'])) echo $templateData['question']->feedback; ?></textarea>
+                <div class="row-fluid">
+                    <div class="span2"><textarea id="fback" name="fback"><?php if(isset($templateData['question'])) echo $templateData['question']->feedback; ?></textarea></div>
+                    <div class="span3"><div class="alert alert-info"><button type="button" class="close" data-dismiss="alert">×</button><?php echo __('Text will automatically appear in response area. Use to give learner a hint or further instruction.'); ?></div></div>
+                </div>
             </div>
         </div>
     </fieldset>
