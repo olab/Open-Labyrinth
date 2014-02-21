b888db772f2a312e6529a35cdb92167c4599c737
Dmitry Semenyuk <dmitry.semenyuk@itrexgroup.com>
2014-02-21 13:40:44 +0200

    Bug fixing: Single step response or action for a QUestion.

--- www/application/views/labyrinth/skin/basic/basic_template.php
+++ www/application/views/labyrinth/skin/basic/basic_template.php
@@ -328,29 +328,29 @@
 
     <body <?php if(isset($templateData['bodyStyle'])) { echo 'style="' . $templateData['bodyStyle'] . '"'; } ?>>
         <?php if (isset($templateData['editor']) and $templateData['editor'] == TRUE) { ?>
-            <script language="javascript" type="text/javascript" src="<?php echo URL::base().'scripts/tinymce/js/tinymce/tinymce.min.js'; ?>"></script>
-            <script language="javascript" type="text/javascript">
-                tinyMCE.init({
-            // General options
-                    mode:"textareas",
-                    relative_urls:false,
-                    skin:"bootstrap",
-                    theme:"advanced",
-                    plugins:"autolink,lists,pagebreak,style,layer,table,save,advhr,advimage,advlink,emotions,iespell,inlinepopups,insertdatetime,preview,media,searchreplace,print,contextmenu,paste,directionality,fullscreen,noneditable,visualchars,nonbreaking,xhtmlxtras,template,wordcount,advlist,autosave,imgmap",
-            // Theme options
-                    theme_advanced_buttons1:"save,newdocument,|,bold,italic,underline,strikethrough,|,justifyleft,justifycenter,justifyright,justifyfull,styleselect,formatselect,fontselect,fontsizeselect,code",
-                    theme_advanced_buttons2:"cut,copy,paste,pastetext,pasteword,|,search,replace,|,bullist,numlist,|,outdent,indent,blockquote,|,undo,redo,|,link,unlink,anchor,image,cleanup,help,|,insertdate,inserttime,preview,|,forecolor,backcolor",
-                    theme_advanced_buttons3:"tablecontrols,|,hr,removeformat,visualaid,|,sub,sup,|,charmap,emotions,iespell,media,advhr,|,print,|,ltr,rtl,|,fullscreen",
-                    theme_advanced_buttons4:"insertlayer,moveforward,movebackward,absolute,|,styleprops,|,cite,abbr,acronym,del,ins,attribs,|,visualchars,nonbreaking,template,pagebreak,restoredraft,|,imgmap",
-                    theme_advanced_toolbar_location:"top",
-                    theme_advanced_toolbar_align:"left",
-                    theme_advanced_statusbar_location:"bottom",
-                    theme_advanced_resizing:true,
-                    editor_selector:"mceEditor",
-                    autocomplete_trigger:"",
-                    entity_encoding: "raw"
-                });
-            </script>
+<!--            <script language="javascript" type="text/javascript" src="--><?php //echo URL::base().'scripts/tinymce/js/tinymce/tinymce.min.js'; ?><!--"></script>-->
+<!--            <script language="javascript" type="text/javascript">-->
+<!--                tinyMCE.init({-->
+<!--            // General options-->
+<!--                    mode:"textareas",-->
+<!--                    relative_urls:false,-->
+<!--                    skin:"bootstrap",-->
+<!--                    theme:"advanced",-->
+<!--                    plugins:"autolink,lists,pagebreak,style,layer,table,save,advhr,advimage,advlink,emotions,iespell,inlinepopups,insertdatetime,preview,media,searchreplace,print,contextmenu,paste,directionality,fullscreen,noneditable,visualchars,nonbreaking,xhtmlxtras,template,wordcount,advlist,autosave,imgmap",-->
+<!--            // Theme options-->
+<!--                    theme_advanced_buttons1:"save,newdocument,|,bold,italic,underline,strikethrough,|,justifyleft,justifycenter,justifyright,justifyfull,styleselect,formatselect,fontselect,fontsizeselect,code",-->
+<!--                    theme_advanced_buttons2:"cut,copy,paste,pastetext,pasteword,|,search,replace,|,bullist,numlist,|,outdent,indent,blockquote,|,undo,redo,|,link,unlink,anchor,image,cleanup,help,|,insertdate,inserttime,preview,|,forecolor,backcolor",-->
+<!--                    theme_advanced_buttons3:"tablecontrols,|,hr,removeformat,visualaid,|,sub,sup,|,charmap,emotions,iespell,media,advhr,|,print,|,ltr,rtl,|,fullscreen",-->
+<!--                    theme_advanced_buttons4:"insertlayer,moveforward,movebackward,absolute,|,styleprops,|,cite,abbr,acronym,del,ins,attribs,|,visualchars,nonbreaking,template,pagebreak,restoredraft,|,imgmap",-->
+<!--                    theme_advanced_toolbar_location:"top",-->
+<!--                    theme_advanced_toolbar_align:"left",-->
+<!--                    theme_advanced_statusbar_location:"bottom",-->
+<!--                    theme_advanced_resizing:true,-->
+<!--                    editor_selector:"mceEditor",-->
+<!--                    autocomplete_trigger:"",-->
+<!--                    entity_encoding: "raw"-->
+<!--                });-->
+<!--            </script>-->
         <?php } ?>
         <?php if(isset($templateData['skin'])) { echo $templateData['skin']; } ?>
 
