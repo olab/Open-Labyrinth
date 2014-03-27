a2f63ff04a6ed12b52cf69604c0d33f54c6c5fd8
a-zharkov <alexander.zharkov@itrexgroup.com>
2014-03-27 11:42:11 +0200

    Fixing SQL script

--- www/updates/v3.1.1_2.sql
+++ www/updates/v3.1.1_2.sql
@@ -1,2 +1,2 @@
-INSERT INTO `openlabyrinth`.`map_question_types` (`id`, `title`, `value`, `template_name`, `template_args`) VALUES ('7', 'Script Concordance Testing', 'sct', 'choice', NULL);
+INSERT INTO `map_question_types` (`id`, `title`, `value`, `template_name`, `template_args`) VALUES ('7', 'Script Concordance Testing', 'sct', 'choice', NULL);
 ALTER TABLE  `webinar_users` ADD  `expert` TINYINT( 1 ) NOT NULL AFTER  `include_4R` ;
