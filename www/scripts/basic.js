79428e8d0c42a2986f03aabb2d2b744be2d382e2
Dmitry Semenyuk <dmitry.semenyuk@itrexgroup.com>
2014-04-07 16:16:19 +0300

    Bug fix: Parallel play of cases - Mode 1 - issues. Textarea.

--- www/scripts/basic.js
+++ www/scripts/basic.js
@@ -1,4 +1,45 @@
 var urlBase = window.location.origin + '/';
+
+$(document).ready(function(){
+    var questions = $('textarea[name^="qresponse_"]'),
+        toNodeHref = '';
+
+    $('a[href^="/renderLabyrinth/go"]').click(function(e){
+        e.preventDefault();
+        toNodeHref = e.currentTarget.href;
+
+        questions.each(function(){
+            var idTextQ = parseInt($(this).prop('name').replace('qresponse_', ''));
+            if ($.inArray(idTextQ, submitTextQ) === -1){
+                ajaxFunction(idTextQ);
+            }
+        });
+    });
+
+    var submitTextQ = [],
+        savedTextQ = 0;
+
+    function ajaxFunction(qid) {
+        submitTextQ.push(qid);
+        var qresp = $("#qresponse_" + qid).val();
+
+        if (qresp != ''){
+            qresp = B64.encode(qresp);
+            var URL = urlBase + "renderLabyrinth/questionResponse/" + qresp + "/" + qid + "/" + idNode;
+
+            var $response = $('#AJAXresponse' + qid);
+            $.get(
+                URL,
+                function(data) {
+                    if(data != '') $response.html(data);
+                    savedTextQ += 1;
+                    if (savedTextQ == questions.length) window.location.href = toNodeHref;
+                }
+            )
+        }
+    }
+});
+
 window.dhx_globalImgPath = urlBase + "scripts/dhtmlxSlider/codebase/imgs/";
 
 function toggle_visibility(id) {
