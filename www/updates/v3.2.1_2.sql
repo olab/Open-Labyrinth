b2ccf69345225d512f332fcfd53bc5cc8f0ef15c
a-zharkov <alexander.zharkov@itrexgroup.com>
2014-11-20 15:13:00 +0200

    Merging SQL files to v3.3

--- www/updates/v3.2.1_2.sql
+++
@@ -1,3 +0,0 @@
-ALTER TABLE  `webinar_maps` ADD  `cumulative` BOOLEAN NOT NULL ;
-INSERT INTO  `map_question_types` ( `id` , `title` , `value` , `template_name` , `template_args` ) VALUES ( NULL ,  'Cumulative',  'area',  'area', NULL );
-ALTER TABLE  `user_sessions` ADD  `notCumulative` BOOLEAN NOT NULL ;
