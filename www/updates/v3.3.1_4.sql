275fd5d5f35bbd187d250f467846964d082b28c2
sergey-rud <sergey.rud@itrexgroup.com>
2015-06-15 10:39:19 +0300

    Turk Talk DB migration

--- www/updates/v3.3.1_4.sql
+++ www/updates/v3.3.1_4.sql
@@ -1,3 +1,16 @@
 INSERT INTO `map_question_types` (`id`, `title`, `value`, `template_name`, `template_args`) VALUES (11, 'Turk Talk', 'ttalk', 'lightarea', NULL);
 ALTER TABLE `map_node_links` ADD COLUMN `hidden` tinyint(1) DEFAULT '0';
 ALTER TABLE `users` ADD COLUMN `settings` text;
+
+CREATE TABLE `webinar_macros` (
+`id` int(11) unsigned NOT NULL,
+  `text` varchar(255) DEFAULT NULL,
+  `hot_keys` varchar(255) DEFAULT NULL,
+  `webinar_id` int(11) unsigned NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+ALTER TABLE `webinar_macros`
+ ADD PRIMARY KEY (`id`), ADD KEY `webinar_id` (`webinar_id`);
+
+ALTER TABLE `webinar_macros`
+MODIFY `id` int(11) unsigned NOT NULL AUTO_INCREMENT;
