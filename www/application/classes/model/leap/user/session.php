edf723b6a2e289980dfbeae263368b1d13ec24a3
sergey-rud <sergey.rud@itrexgroup.com>
2016-03-15 10:44:38 +0200

    phpDoc enhancements

--- www/application/classes/model/leap/user/session.php
+++ www/application/classes/model/leap/user/session.php
@@ -31,10 +31,10 @@ defined('SYSPATH') or die('No direct script access.');
  * @property bool $notCumulative
  * @property string $user_ip
  * @property Model_Leap_User $user
- * @property Model_Leap_User_SessionTrace[] $traces
+ * @property Model_Leap_User_SessionTrace[]|DB_ResultSet $traces
  * @property Model_Leap_Map $map
- * @property Model_Leap_User_Response[] $responses
- * @property Model_Leap_Statement[] $statements
+ * @property Model_Leap_User_Response[]|DB_ResultSet $responses
+ * @property Model_Leap_Statement[]|DB_ResultSet $statements
  */
 class Model_Leap_User_Session extends DB_ORM_Model {
 
