6388f363fac3e79604269e45bf9f25a4c1e36975
sergey-rud <sergey.rud@itrexgroup.com>
2016-04-21 19:40:02 +0300

    H5P: improved UI

--- www/application/views/h5p/contentShow.php
+++ www/application/views/h5p/contentShow.php
@@ -2,6 +2,7 @@
 /** @var array $content */
 /** @var string $embed_code */
 /** @var string $messages */
+/** @var string $settings */
 /** @var bool $has_errors */
 ?>
 
@@ -20,30 +21,46 @@
         </h2>
         <div class="h5p-wp-admin-wrapper">
 
-            <div class="h5p-content-wrap">
-                <?php print $embed_code; ?>
-            </div>
-
-            <?php if (false): ?>
-                <div class="postbox h5p-sidebar">
-                    <h2><?php echo __('Shortcode'); ?></h2>
-                    <div class="h5p-action-bar-settings h5p-panel">
-                        <p><?php echo __("What's next?"); ?></p>
-                        <p><?php echo __('You can use the following shortcode to insert this interactive content into nodes'); ?></p>
-                        <code>[h5p id="<?php print $content['id'] ?>"]</code>
+            <div class="row">
+                <div class="span8">
+                    <div class="h5p-content-wrap">
+                        <?php print $embed_code; ?>
                     </div>
                 </div>
-            <?php endif; ?>
 
-            <div class="postbox h5p-sidebar">
-                <h2><?php echo __('Tags'); ?></h2>
-                <div class="h5p-action-bar-settings h5p-panel">
-                    <?php if (empty($content['tags'])): ?>
-                        <p style="font-style: italic;"><?php echo __('No tags'); ?></p>
-                    <?php else: ?>
-                        <p><?php echo $content['tags']; ?></p>
+                <div class="span4">
+                    <?php if (true): ?>
+                        <div class="postbox h5p-sidebar panel">
+                            <div class="panel-heading">
+                                <b><?php echo __('Shortcode'); ?></b>
+                            </div>
+                            <div class="panel-body">
+                                <div class="h5p-action-bar-settings h5p-panel">
+                                    <p><?php echo __("What's next?"); ?></p>
+                                    <p><?php echo __('You can use the following shortcode to insert this interactive content into nodes:'); ?></p>
+                                    <code>[[H5P:<?php print $content['id'] ?>]]</code>
+                                </div>
+                            </div>
+                        </div>
+                        <br>
                     <?php endif; ?>
+
+                    <div class="postbox h5p-sidebar panel">
+                        <div class="panel-heading">
+                            <b><?php echo __('Tags'); ?></b>
+                        </div>
+                        <div class="panel-body">
+                            <div class="h5p-action-bar-settings h5p-panel">
+                                <?php if (empty($content['tags'])): ?>
+                                    <p style="font-style: italic;"><?php echo __('No tags'); ?></p>
+                                <?php else: ?>
+                                    <p><?php echo $content['tags']; ?></p>
+                                <?php endif; ?>
+                            </div>
+                        </div>
+                    </div>
                 </div>
+
             </div>
 
         </div>
