2ea601a3d6170b3c8c9d2f5f7e47c3ff7d9a641f
sergey-rud <sergey.rud@itrexgroup.com>
2016-04-28 12:49:16 +0300

    refactor

--- www/application/classes/h5pcontentadmin.php
+++ www/application/classes/h5pcontentadmin.php
@@ -1,20 +1,24 @@
 <?php
 /**
- * H5P Plugin.
+ * Open Labyrinth [ http://www.openlabyrinth.ca ]
  *
- * @package   H5P
- * @author    Joubel <contact@joubel.com>
- * @license   MIT
- * @link      http://joubel.com
- * @copyright 2014 Joubel
+ * Open Labyrinth is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * Open Labyrinth is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with Open Labyrinth.  If not, see <http://www.gnu.org/licenses/>.
+ *
+ * @copyright Copyright 2012 Open Labyrinth. All Rights Reserved.
+ *
  */
 
-/**
- * H5P Content Admin class
- *
- * @package H5PPluginAdmin
- * @author Joubel <contact@joubel.com>
- */
 class H5PContentAdmin
 {
 
