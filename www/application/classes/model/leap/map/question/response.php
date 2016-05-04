eef85fd2d9602c3cce7ce1834feac8b09a471e20
sergey-rud <sergey.rud@itrexgroup.com>
2016-05-04 14:51:40 +0300

    added phpDoc class description, code reformat

--- www/application/classes/model/leap/map/question/response.php
+++ www/application/classes/model/leap/map/question/response.php
@@ -21,73 +21,87 @@
 defined('SYSPATH') or die('No direct script access.');
 
 /**
- * Model for map_question_responces table in database 
+ * @property int $id
+ * @property int $question_id
+ * @property int $is_correct
+ * @property int $score
+ * @property int $order
+ * @property string $response
+ * @property string $feedback
+ * @property string $from
+ * @property string $to
  */
-class Model_Leap_Map_Question_Response extends DB_ORM_Model {
+class Model_Leap_Map_Question_Response extends DB_ORM_Model
+{
 
-    public function __construct() {
+    public function __construct()
+    {
         parent::__construct();
 
         $this->fields = array(
             'id' => new DB_ORM_Field_Integer($this, array(
                 'max_length' => 11,
-                'nullable' => FALSE,
-                'unsigned' => TRUE,
+                'nullable' => false,
+                'unsigned' => true,
             )),
             'question_id' => new DB_ORM_Field_Integer($this, array(
                 'max_length' => 11,
-                'nullable' => FALSE,
+                'nullable' => false,
             )),
             'response' => new DB_ORM_Field_String($this, array(
                 'max_length' => 250,
-                'nullable' => FALSE,
-                'savable' => TRUE,
+                'nullable' => false,
+                'savable' => true,
             )),
             'feedback' => new DB_ORM_Field_Text($this, array(
-                'nullable' => FALSE,
-                'savable' => TRUE,
+                'nullable' => false,
+                'savable' => true,
             )),
             'is_correct' => new DB_ORM_Field_Integer($this, array(
                 'max_length' => 4,
-                'nullable' => FALSE,
-                'savable' => TRUE,
+                'nullable' => false,
+                'savable' => true,
             )),
             'score' => new DB_ORM_Field_Integer($this, array(
                 'max_length' => 11,
-                'nullable' => FALSE,
+                'nullable' => false,
             )),
             'from' => new DB_ORM_Field_String($this, array(
                 'max_length' => 200,
-                'nullable' => TRUE,
-                'savable' => TRUE,
+                'nullable' => true,
+                'savable' => true,
             )),
             'to' => new DB_ORM_Field_String($this, array(
                 'max_length' => 200,
-                'nullable' => TRUE,
-                'savable' => TRUE,
+                'nullable' => true,
+                'savable' => true,
             )),
             'order' => new DB_ORM_Field_Integer($this, array(
                 'max_length' => 10,
-                'nullable' => FALSE,
-                'unsigned' => TRUE,
+                'nullable' => false,
+                'unsigned' => true,
                 'default' => 0
             )),
         );
     }
 
-    public static function data_source() {
+    public static function data_source()
+    {
         return 'default';
     }
 
-    public static function table() {
+    public static function table()
+    {
         return 'map_question_responses';
     }
 
-    public static function primary_key() {
+    public static function primary_key()
+    {
         return array('id');
     }
 
-    public function addFullResponses($questionId, $values){
+    public function addFullResponses($questionId, $values)
+    {
         $this->question_id = $questionId;
         $this->response = Arr::get($values, 'response', '');
         $this->feedback = Arr::get($values, 'feedback', '');
@@ -95,37 +109,41 @@ class Model_Leap_Map_Question_Response extends DB_ORM_Model {
         $this->score = Arr::get($values, 'score', 0);
 
         $this->save();
+
         return true;
     }
 
-    public function getResponsesByQuestion($questionId) {
+    public function getResponsesByQuestion($questionId)
+    {
         $builder = DB_SQL::select('default')->from($this->table())->where('question_id', '=', (int)$questionId);
         $result = $builder->query();
-        
-        if($result->is_loaded()) {
+
+        if ($result->is_loaded()) {
             $responses = array();
-            foreach($result as $record) {
+            foreach ($result as $record) {
                 $responses[] = DB_ORM::model('map_question_response', array((int)$record['id']));
             }
-            
+
             return $responses;
         }
-        return NULL;
+
+        return null;
     }
-    
-    public function updateResponses($questionId, $values) {
+
+    public function updateResponses($questionId, $values)
+    {
         $responses = $this->getResponsesByQuestion($questionId);
 
         $responsesFromJSON = $this->prepareResponsesFromJSON(Arr::get($values, 'responses', null));
 
-        if($responses != NULL && count($responses) > 0) {
-            foreach($responses as $response) {
-                if($responsesFromJSON != null && isset($responsesFromJSON['old']) && isset($responsesFromJSON['old'][$response->id])) {
-                    $response->response   = Arr::get($responsesFromJSON['old'][$response->id], 'response', '');
-                    $response->feedback   = Arr::get($responsesFromJSON['old'][$response->id], 'feedback', '');
+        if ($responses != null && count($responses) > 0) {
+            foreach ($responses as $response) {
+                if ($responsesFromJSON != null && isset($responsesFromJSON['old']) && isset($responsesFromJSON['old'][$response->id])) {
+                    $response->response = Arr::get($responsesFromJSON['old'][$response->id], 'response', '');
+                    $response->feedback = Arr::get($responsesFromJSON['old'][$response->id], 'feedback', '');
                     $response->is_correct = Arr::get($responsesFromJSON['old'][$response->id], 'correctness', 2);
-                    $response->score      = Arr::get($responsesFromJSON['old'][$response->id], 'score', 0);
-                    $response->order      = Arr::get($responsesFromJSON['old'][$response->id], 'order', 1);
+                    $response->score = Arr::get($responsesFromJSON['old'][$response->id], 'score', 0);
+                    $response->order = Arr::get($responsesFromJSON['old'][$response->id], 'order', 1);
 
                     $response->save();
                 } else {
@@ -134,38 +152,53 @@ class Model_Leap_Map_Question_Response extends DB_ORM_Model {
             }
         }
 
-        if($responsesFromJSON != null && isset($responsesFromJSON['new']) && count($responsesFromJSON['new']) > 0) {
-            foreach($responsesFromJSON['new'] as $response) {
+        if ($responsesFromJSON != null && isset($responsesFromJSON['new']) && count($responsesFromJSON['new']) > 0) {
+            foreach ($responsesFromJSON['new'] as $response) {
                 DB_ORM::insert('map_question_response')
-                        ->column('question_id', $questionId)
-                        ->column('response', Arr::get($response, 'response', ''))
-                        ->column('feedback', Arr::get($response, 'feedback', ''))
-                        ->column('is_correct', Arr::get($response, 'correctness', 2))
-                        ->column('score', Arr::get($response, 'score', 0))
-                        ->column('order', Arr::get($response, 'order', 1))
-                        ->execute();
+                    ->column('question_id', $questionId)
+                    ->column('response', Arr::get($response, 'response', ''))
+                    ->column('feedback', Arr::get($response, 'feedback', ''))
+                    ->column('is_correct', Arr::get($response, 'correctness', 2))
+                    ->column('score', Arr::get($response, 'score', 0))
+                    ->column('order', Arr::get($response, 'order', 1))
+                    ->execute();
             }
         }
     }
 
-    private function prepareResponsesFromJSON($responses) {
-        if($responses == null) return null;
+    private function prepareResponsesFromJSON($responses)
+    {
+        if ($responses == null) {
+            return null;
+        }
 
         $result = array();
-        foreach($responses as $responseJSON) {
+        foreach ($responses as $responseJSON) {
             $object = json_decode($responseJSON, true);
 
-            if($object == null) { continue; }
+            if ($object == null) {
+                continue;
+            }
 
             $responseData = array();
 
-            if(isset($object['response']))    { $responseData['response']    = urldecode(str_replace('+', '&#43;', base64_decode($object['response']))); }
-            if(isset($object['feedback']))    { $responseData['feedback']    = urldecode(str_replace('+', '&#43;', base64_decode($object['feedback']))); }
-            if(isset($object['correctness'])) { $responseData['correctness'] = $object['correctness']; }
-            if(isset($object['score']))       { $responseData['score']       = $object['score'];       }
-            if(isset($object['order']))       { $responseData['order']       = $object['order'];       }
+            if (isset($object['response'])) {
+                $responseData['response'] = urldecode(str_replace('+', '&#43;', base64_decode($object['response'])));
+            }
+            if (isset($object['feedback'])) {
+                $responseData['feedback'] = urldecode(str_replace('+', '&#43;', base64_decode($object['feedback'])));
+            }
+            if (isset($object['correctness'])) {
+                $responseData['correctness'] = $object['correctness'];
+            }
+            if (isset($object['score'])) {
+                $responseData['score'] = $object['score'];
+            }
+            if (isset($object['order'])) {
+                $responseData['order'] = $object['order'];
+            }
 
-            if(isset($object['id']) && $object['id'] > 0) {
+            if (isset($object['id']) && $object['id'] > 0) {
                 $responseData['id'] = $object['id'];
                 $result['old'][$object['id']] = $responseData;
             } else {
@@ -176,30 +209,35 @@ class Model_Leap_Map_Question_Response extends DB_ORM_Model {
         return $result;
     }
 
-    public function deleteByQuestion($questionId) {
+    public function deleteByQuestion($questionId)
+    {
         $builder = DB_ORM::delete('map_question_response')->where('question_id', '=', (int)$questionId);
         $builder->execute();
     }
-    
-    public function duplicateResponses($fromQuestionId, $toQuestionId) {
+
+    public function duplicateResponses($fromQuestionId, $toQuestionId)
+    {
         $resp = $this->getResponsesByQuestion($fromQuestionId);
-        
-        if($resp == null || $toQuestionId == null || $toQuestionId <= 0) return;
-        
-        foreach($resp as $r) {
+
+        if ($resp == null || $toQuestionId == null || $toQuestionId <= 0) {
+            return;
+        }
+
+        foreach ($resp as $r) {
             $builder = DB_ORM::insert('map_question_response')
-                    ->column('question_id', $toQuestionId)
-                    ->column('response', $r->response)
-                    ->column('feedback', $r->feedback)
-                    ->column('is_correct', $r->is_correct)
-                    ->column('score', $r->score);
-            
+                ->column('question_id', $toQuestionId)
+                ->column('response', $r->response)
+                ->column('feedback', $r->feedback)
+                ->column('is_correct', $r->is_correct)
+                ->column('score', $r->score);
+
             $builder->execute();
         }
     }
 
     public function exportMVP($questionId)
     {
-        return DB_SQL::select('default')->from('map_question_responses')->where('question_id', '=', $questionId)->query()->as_array();
+        return DB_SQL::select('default')->from('map_question_responses')->where('question_id', '=',
+            $questionId)->query()->as_array();
     }
 }
