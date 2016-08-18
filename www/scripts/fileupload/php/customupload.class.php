2942b9975d7642add04bc4150bd5e5e9dac82b3d
sergey-rud <sergey.rud@itrexgroup.com>
2016-08-18 20:05:38 +0300

    UploadHandler configuration improvements

--- www/scripts/fileupload/php/customupload.class.php
+++ www/scripts/fileupload/php/customupload.class.php
@@ -6,6 +6,9 @@ require_once 'upload.class.php';
 
 class CustomUploadHandler extends UploadHandler
 {
+    /**
+     * @var array
+     */
     public static $allowedMimeTypes = [
         'text/plain' => 'txt',
         'text/csv' => 'csv',
@@ -55,6 +58,70 @@ class CustomUploadHandler extends UploadHandler
         'video/x-flv' => 'flv',
     ];
 
+    
+    /**
+     * CustomUploadHandler constructor.
+     * @param null|array $options
+     */
+    public function __construct($options = null)
+    {
+        $this->options = array(
+            'script_url' => $this->getFullUrl() . '/',
+            'upload_dir' => DOCROOT . 'scripts/fileupload/php/files/',
+            'upload_url' => $this->getFullUrl() . '/files/',
+            'param_name' => 'files',
+            // Set the following option to 'POST', if your server does not support
+            // DELETE requests. This is a parameter sent to the client:
+            'delete_type' => 'DELETE',
+            // The php.ini settings upload_max_filesize and post_max_size
+            // take precedence over the following max_file_size setting:
+            'max_file_size' => null,
+            'min_file_size' => 1,
+            'accept_file_types' => '/.+$/i',
+            // The maximum number of files for the upload directory:
+            'max_number_of_files' => null,
+            // Image resolution restrictions:
+            'max_width' => null,
+            'max_height' => null,
+            'min_width' => 1,
+            'min_height' => 1,
+            // Set the following option to false to enable resumable uploads:
+            'discard_aborted_uploads' => true,
+            // Set to true to rotate images based on EXIF meta data, if available:
+            'orient_image' => false,
+            'image_versions' => array(
+                // Uncomment the following version to restrict the size of
+                // uploaded images. You can also add additional versions with
+                // their own upload directories:
+                /*
+                'large' => array(
+                    'upload_dir' => dirname($_SERVER['SCRIPT_FILENAME']).'/files/',
+                    'upload_url' => $this->getFullUrl().'/files/',
+                    'max_width' => 1920,
+                    'max_height' => 1200,
+                    'jpeg_quality' => 95
+                ),
+                */
+                'thumbnail' => array(
+                    'upload_dir' => DOCROOT . 'scripts/fileupload/php/thumbnails/',
+                    'upload_url' => $this->getFullUrl() . '/thumbnails/',
+                    'max_width' => 80,
+                    'max_height' => 80
+                )
+            )
+        );
+        if ($options) {
+            $this->options = array_replace_recursive($this->options, $options);
+        }
+    }
+
+    /**
+     * @param $uploaded_file
+     * @param $file
+     * @param $error
+     * @param $index
+     * @return bool
+     */
     protected function validate($uploaded_file, $file, $error, $index)
     {
         if (!parent::validate($uploaded_file, $file, $error, $index)) {
@@ -71,6 +138,14 @@ class CustomUploadHandler extends UploadHandler
         }
 
         return true;
+    }
+
+    /**
+     * @return string
+     */
+    protected function getFullUrl()
+    {
+        return URL::base(true) . 'scripts/fileupload/php';
     }
 
     /**
